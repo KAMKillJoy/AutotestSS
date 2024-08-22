644e9f41a3ee94779ca7b3fa4dc60cff29e98cbf
John Doe <kamkilljoy@gmail.com>
2024-08-22 10:02:31 +0400

    Все ассерты теперь работают корректно.

--- Tests.py
+++ Tests.py
@@ -18,5 +18,23 @@ def test_student_registration_form(browser):
     student_registration_form.enter_state(Students.StudentVasia["state"])
     student_registration_form.enter_city(Students.StudentVasia["city"])
     student_registration_form.click_submit()
-    assert (student_registration_form.read_result_modal_student_name() ==
-            f"{Students.StudentVasia["firstname"]} {Students.StudentVasia["lastname"]}")
+
+    assert (student_registration_form.read_result_modal_student_name().casefold() ==
+            f"{Students.StudentVasia["firstname"]} {Students.StudentVasia["lastname"]}".casefold())
+    assert (student_registration_form.read_result_modal_student_email().casefold() ==
+            Students.StudentVasia["email"].casefold())
+    assert (student_registration_form.read_result_modal_student_gender().casefold() ==
+            Students.StudentVasia["gender"].casefold())
+    assert (student_registration_form.read_result_modal_student_mobile().casefold() ==
+            Students.StudentVasia["mobile"].casefold())
+    assert (student_registration_form.read_result_modal_student_date_of_birth().casefold() ==
+            f"{Students.StudentVasia['dateofbirth'][0]} {Students.StudentVasia['dateofbirth'][1]},"
+            f"{Students.StudentVasia['dateofbirth'][2]}".casefold())
+    assert (student_registration_form.read_result_modal_student_subjects().casefold() ==
+            ", ".join(Students.StudentVasia['subjects']).casefold())
+    assert (student_registration_form.read_result_modal_student_picture().casefold() ==
+            Students.StudentVasia["picture"].casefold())
+    assert (student_registration_form.read_result_modal_student_address().casefold() ==
+            Students.StudentVasia["address"].casefold())
+    assert (student_registration_form.read_result_modal_state_and_city().casefold() ==
+            f"{Students.StudentVasia['state']} {Students.StudentVasia['city']}".casefold())
